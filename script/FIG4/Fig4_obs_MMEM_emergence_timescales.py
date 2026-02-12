87643a5d1f731e86b01f407fe9e3a59f5cdd3bb9
m301036 <josie.aruhasi@mpimet.mpg.de>
2026-02-12 21:50:43 +0100

    uPdates

--- script/FIG4/Fig4_obs_MMEM_emergence_timescales.py
+++ script/FIG4/Fig4_obs_MMEM_emergence_timescales.py
@@ -3,32 +3,23 @@ import xarray as xr
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import numpy as np
-# import all the plotting packages
-from src.plot_func import *
 # %%
-# import importlib
-# importlib.reload(src.plot_func)
-# call once at the start of your script / notebook:
-set_science_advances_style(column='double')  # or 'single'
-# %%
-dir_in = "/work/mh0033/m301036/OBS_LPS_revision/docs/data/FIG4/"
-data_obs = xr.open_dataset(dir_in + "OBS_Emergence_time_scale.nc")
+data_obs = xr.open_dataset("/work/mh0033/m301036/OBS_LPS_revision/docs/data/FIG4/median_SNgt1/OBS_Emergence_time_scale.nc")
 data_obs = data_obs.fillna(75)
 # %%
 # input monotonicity 
-dir_in = '/work/mh0033/m301036/OBS_LPS_revision/docs/data/FIG4/monotonicity/'
+dir_in = '/work/mh0033/m301036/Land_surf_temp/Disentangling_OBS_SAT_trend/Revised_main_figures/Figure4_Emergence_timescale/data/'
 monotonic_test = xr.open_dataset(dir_in+'obs_emergence_monotonicity.nc')
 # %%
 # rename the variable name
-data_obs = data_obs.rename_vars({'__xarray_dataarray_variable__': 'emergence_timescale_mean'})
+# data_obs = data_obs.rename_vars({'__xarray_dataarray_variable__': 'emergence_timescale_mean'})
 # %%
-# %%
-dir_MMLE = "/work/mh0033/m301036/OBS_LPS_revision/docs/data/FIG4/"
-MMEM = xr.open_dataset(dir_MMLE + "MMEM_emergence_timescale_mean.nc")
+dir_MMLE = "/work/mh0033/m301036/OBS_LPS_revision/docs/data/FIG4/median_SNgt1/"
+MMEM = xr.open_dataset(dir_MMLE + "MMEM_emergence_timescale_median.nc")
 # %%
 # check the NaN values
-print(data_obs.emergence_timescale_mean.isnull().sum())
-# print(MPI_ENS.emergence_timescale_mean.isnull().sum())
+print(data_obs.emergence_timescale.isnull().sum())
+print(MMEM.emergence_timescale_median.isnull().sum())
 # %%
 # def skip_NaN_minus(data, data_minus):
 #   data_minus = data_minus.where(data != -1)
@@ -64,8 +55,10 @@ from matplotlib.colors import BoundaryNorm, ListedColormap
 # Replace invalid values with NaN for consistent masking
 data_obs = data_obs.where(data_obs != -1)
 MMEM = MMEM.where(MMEM != -1)
-diff = MMEM - data_obs
+# Compute difference on the matching variables so we can plot a DataArray
+diff = MMEM['emergence_timescale_median'] - data_obs['emergence_timescale']
 diff = diff.where(diff.notnull())  # Mask invalid data
+
 # # Define levels and normalization for OBS
 # levels_obs = np.arange(10, 85, 5)  # Extend to 85 to allow the upper bound
 # cmap_obs = plt.get_cmap("Spectral")
@@ -80,10 +73,21 @@ diff = diff.where(diff.notnull())  # Mask invalid data
 # custom_cmap_diff = ListedColormap(colors_diff)
 # norm_diff = BoundaryNorm(levels_diff, ncolors=len(colors_diff), extend="both")
 # %%
+plt.rcParams['figure.figsize'] = (8, 10)
+plt.rcParams['font.size'] = 16
+# plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['axes.labelsize'] = 16
+plt.rcParams['ytick.direction'] = 'out'
+plt.rcParams['ytick.minor.visible'] = True
+plt.rcParams['ytick.major.right'] = True
+plt.rcParams['ytick.right'] = True
+plt.rcParams['xtick.bottom'] = True
+plt.rcParams['savefig.transparent'] = True
+
 import matplotlib.colors as mcolors
 import palettable
 import cartopy.util as cutil
-
+# %%
 # Define levels and colors
 # levels_obs = np.arange(10, 80, 5)
 # cmap_obs = plt.get_cmap("Spectral")
@@ -93,7 +97,7 @@ import cartopy.util as cutil
 
 # # Update BoundaryNorm
 # norm_obs = BoundaryNorm(levels_obs, ncolors=len(levels_obs), extend="max")
-
+# %%
 # levels_obs = np.append(np.arange(10, 75, 5), 75)  # [10, 15, ..., 75, 80]
 # cmap_base = plt.get_cmap('Spectral') #'OrRd_r'
 # colors = cmap_base(np.linspace(0, 1, len(levels_obs) - 1))  # One less than number of edges
@@ -101,7 +105,6 @@ import cartopy.util as cutil
 # custom_cmap = ListedColormap(colors)
 # norm = BoundaryNorm(levels_obs, ncolors=len(levels_obs), extend='max')
 
-# levels_obs = np.arange(10, 80, 5)  # 10 to 75OrRd_r
 levels_obs = np.array([10, 15, 20, 25, 30, 35,
                        40, 45, 50, 55, 60, 65, 70, 73])
 
@@ -115,13 +118,6 @@ custom_cmap.set_over('white')
 
 # BoundaryNorm with an over-bin
 norm = BoundaryNorm(levels_obs, ncolors=custom_cmap.N, extend='max')
-
-# cmap = mcolors.ListedColormap(palettable.cmocean.sequential.Solar_17.mpl_colors)
-# cmap = mcolors.ListedColormap(palettable.matplotlib.Plasma_17.mpl_colors)
-# cmap = mcolors.ListedColormap(palettable.scientific.sequential.LaJolla_20.mpl_colors[::-1])
-# cmap_diff=mcolors.ListedColormap(palettable.cmocean.diverging.Balance_20.mpl_colors[::-1])
-# reverse the color map
-# cmap_r = mcolors.ListedColormap(palettable.cmocean.sequential.Amp_17.mpl_colors[::-1])
 # Create the figure
 fig = plt.figure(figsize=(20, 15))
 gs = plt.GridSpec(1, 2)
@@ -142,7 +138,7 @@ gl.bottom_labels = True
 gl.left_labels = True
 
 # Plot data
-p_obs = data_obs['emergence_timescale_mean'].plot(
+p_obs = data_obs['emergence_timescale'].plot(
     ax=ax,
     transform=ccrs.PlateCarree(),
     cmap=custom_cmap,
@@ -158,9 +154,10 @@ monotonic_with_cyclic, lon_with_cyclic = cutil.add_cyclic_point(monotonic_test['
 
 # Overlay the monotonicity mask as hatching (False = not monotonic)
 ax.contourf(
-    lon_with_cyclic, data_obs['emergence_timescale_mean'].lat, ~monotonic_with_cyclic,
+    lon_with_cyclic, data_obs['emergence_timescale'].lat, ~monotonic_with_cyclic,
     levels=[0.5, 1.5], hatches=['///'], colors='none', transform=ccrs.PlateCarree(), alpha=0
 )
+# Add colorbar
 # Add colorbar
 ticks_obs = levels_obs  # or a subset if you want fewer labels
 
@@ -173,8 +170,8 @@ cbar_obs = plt.colorbar(
     extend='max'         # <<< important: show the white ">" wedge
 )
 cbar_obs = plt.colorbar(p_obs, cax=cbar_ax_obs, orientation='horizontal', ticks=levels_obs)
-cbar_obs.set_label('Emergence timescale with S/N > 2\n(year)', fontsize=20, loc='center')
-cbar_obs.ax.tick_params(labelsize=18)   #, labelrotation=45
+cbar_obs.set_label('Emergence timescale with S/N > 1\n(year)', fontsize=20, loc='center')
+cbar_obs.ax.tick_params(labelsize=18)   #labelrotation=45
 cbar_obs.ax.tick_params(direction='out', length=8, width=2)
 cbar_obs.ax.tick_params(which='minor', bottom=False, top=False, length=0)
 # add double legends to the colorbar: below the upper one denote the start year of the signal segments
@@ -231,10 +228,12 @@ cmap_diff = "RdBu_r"
 # Define levels and calculate number of color bins (12 bins from 13 boundaries)
 # cmap_diff = mcolors.ListedColormap(palettable.cmocean.diverging.Curl_20.mpl_colors)  # Use a diverging colormap for differences
 # cmap_diff = palettable.cmocean.diverging.Balance_20.mpl_colormap
-levels_diff = np.arange(-35, 36, 5)  # Symmetric around 0
+# levels_diff = np.arange(-35, 36, 5)  # Symmetric around 0
+# n_bins = len(levels_diff)-2         # = 10 bins
+levels_diff = np.arange(-25, 30, 5)  # Symmetric around 0
 n_bins = len(levels_diff)-2         # = 10 bins
 
-p_diff = diff['emergence_timescale_mean'].plot(ax=ax1, transform=ccrs.PlateCarree(), cmap=cmap_diff, levels=levels_diff, add_colorbar=False)
+p_diff = diff.plot(ax=ax1, transform=ccrs.PlateCarree(), cmap=cmap_diff, levels=levels_diff, add_colorbar=False)
 # Add title
 ax1.set_title("B", loc='left', fontsize=28, pad=10, color='black',
               fontweight='bold')
@@ -243,10 +242,10 @@ ax1.text(0.5, 1.05, 'MMLE - OBS Difference', transform=ax1.transAxes, fontsize=2
 # Add colorbar for DIFF
 cbar_ax_diff = fig.add_axes([0.58, 0.32, 0.3, 0.025])
 cbar_diff = plt.colorbar(p_diff, cax=cbar_ax_diff, orientation='horizontal', 
-                         ticks=levels_diff, extend='min')
+                         ticks=levels_diff, extend='min') 
 cbar_diff.set_label('Emergence timescale difference\n(year)', fontsize=20, loc='center')
 # the label shows every 2 other tick
-cbar_diff.ax.tick_params(labelsize=18, labelrotation=45)
+cbar_diff.ax.tick_params(labelsize=18)  #, labelrotation=45
 cbar_diff.ax.tick_params(direction='out', length=8, width=2)  
 cbar_diff.ax.tick_params(which='minor', bottom=False, top=False, length=0)
 
@@ -275,7 +274,7 @@ cbar_ax_diff2.annotate(
         arrowprops=dict(arrowstyle="<|-", lw=2.0, mutation_scale=20)
 )
 cbar_ax_diff2.text(
-        0.25, -1.22, "MMLE Speedy",
+        0.25, -1.22, "MMLE too soon",
     transform=cbar_ax_diff2.transAxes,
     ha="center", va="bottom", fontsize=18, color='#3F60A9'
 )
@@ -288,7 +287,7 @@ cbar_ax_diff2.annotate(
         arrowprops=dict(arrowstyle="-|>", lw=2.5, mutation_scale=20, capstyle='round')
 )
 cbar_ax_diff2.text(
-        0.75, -1.22, "MMLE Tardy",
+        0.75, -1.22, "MMLE too late",
     transform=cbar_ax_diff2.transAxes,
     ha="center", va="bottom", fontsize=18, color='#CE2826'
 )
@@ -298,12 +297,12 @@ cbar_ax_diff2.text(
 arctic_lon_mid = (0 + 360) / 2
 arctic_lat_mid = (66.5 + 90) / 2
 ax.plot([0, 360, 360, 0, 0], [66.5, 66.5, 90, 90, 66.5],
-        color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
+        color='grey', linewidth=2.5, transform=ccrs.PlateCarree())
 ax.text(arctic_lon_mid, arctic_lat_mid, 'ARC', color='white', fontsize=22,transform=ccrs.PlateCarree(),
         ha='center', va='center')  # Label for Arctic
 ax1.plot([0, 360, 360, 0, 0], [66.5, 66.5, 90, 90, 66.5],
-        color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
-# ax1.text(arctic_lon_mid, arctic_lat_mid, 'ARC', color='gray', fontsize=22, fontweight='bold',transform=ccrs.PlateCarree(),
+        color='grey', linewidth=2.5, transform=ccrs.PlateCarree())
+# ax1.text(arctic_lon_mid, arctic_lat_mid, 'ARC', color='grey', fontsize=22, fontweight='bold',transform=ccrs.PlateCarree(),
 #         ha='center', va='center')  # Label for Arctic
 
 # WH box
@@ -311,64 +310,58 @@ wh_lon_mid = (310 + 350) / 2
 wh_lat_mid = (42 + 60) / 2
 box_lons = np.array([310, 350, 350, 310, 310])
 box_lats = np.array([42, 42, 60, 60, 42])
-ax.plot(box_lons, box_lats, color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
+ax.plot(box_lons, box_lats, color='grey', linewidth=2.5, transform=ccrs.PlateCarree())
 ax.text(wh_lon_mid, wh_lat_mid, 'NAWH', color='Black', fontsize=22,  transform=ccrs.PlateCarree(),
         ha='center', va='center')  # Label for WH
-ax1.plot(box_lons, box_lats, color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
-# ax1.text(wh_lon_mid, wh_lat_mid, 'NAWH', color='gray', fontsize=22, fontweight='bold',transform=ccrs.PlateCarree(),
+ax1.plot(box_lons, box_lats, color='grey', linewidth=2.5, transform=ccrs.PlateCarree())
+# ax1.text(wh_lon_mid, wh_lat_mid, 'NAWH', color='grey', fontsize=22, fontweight='bold',transform=ccrs.PlateCarree(),
 #         ha='center', va='center')  # Label for WH
 
 # Southeast Pacific box
 sep_lon_mid = (200 + 320) / 2
 sep_lat_mid = (0 + -25) / 2
 ax.plot([200%360, 280%360, 280%360, 250%360, 200%360], [0, 0, -25, -25, 0],
-        color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
+        color='grey', linewidth=2.5, transform=ccrs.PlateCarree())
 ax.text(sep_lon_mid, sep_lat_mid, 'SEP', color='black', fontsize=22, transform=ccrs.PlateCarree(),
         ha='center', va='center')  # Label for SEP
 ax1.plot([200%360, 280%360, 280%360, 250%360, 200%360], [0, 0, -25, -25, 0],
-        color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
+        color='grey', linewidth=2.5, transform=ccrs.PlateCarree())
 # ax1.text(sep_lon_mid, sep_lat_mid, 'SEP', color='black', fontsize=22, fontweight='bold', transform=ccrs.PlateCarree(),
 #         ha='center', va='center')  # Label for SEP
-# # SO box 
-# so_lon_mid = (0 + 360) / 2
-# so_lat_mid = (-65 + -50) / 2
-# ax.plot([0, 360, 360, 0, 0], [-70, -70, -50, -50, -70],
-#         color='gray', linewidth=2.0, transform=ccrs.PlateCarree())
-# ax.text(so_lon_mid, so_lat_mid, 'SO', color='black', fontsize=18, transform=ccrs.PlateCarree(),
-#         ha='center', va='center')  # Label for SO
+
 # Extratropical South Pacific box
-sop_lon_mid = (230 + 280) / 2
-sop_lat_mid = (-40 + -70) / 2
-ax.plot([230%360, 280%360, 280%360, 230%360, 230%360], [-40, -40, -70, -70, -40],
-        color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
+sop_lon_mid = (180 + 260) / 2
+sop_lat_mid = (-70 + -60) / 2
+ax.plot([180%360, 260%360, 260%360, 180%360, 180%360], [-55, -55, -70, -70, -55],
+        color='grey', linewidth=2.5, transform=ccrs.PlateCarree())
 ax.text(sop_lon_mid, sop_lat_mid, 'SOP', color='black', fontsize=22, transform=ccrs.PlateCarree(),
         ha='center', va='center')  # Label for SOP
-ax1.plot([230%360, 280%360, 280%360, 230%360, 230%360], [-40, -40, -70, -70, -40],
-        color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
-# North Pacific box
-npac_lon_mid = (175 + 220) / 2
-npac_lat_mid = (30 + 50) / 2
-ax.plot([175%360, 220%360, 220%360, 175%360, 175%360], [30, 30, 50, 50, 30],
-        color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
-ax.text(npac_lon_mid, npac_lat_mid, 'NPM', color='black', fontsize=22, transform=ccrs.PlateCarree(),
-        ha='center', va='center')  # Label for NPM 
-ax1.plot([175%360, 220%360, 220%360, 175%360, 175%360], [30, 30, 50, 50, 30],
-        color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
-# # Extratropical South Pacific box [depercated for SA_round1_revision]
-# sop_lon_mid = (180 + 260) / 2
-# sop_lat_mid = (-70 + -60) / 2
-# ax.plot([180%360, 260%360, 260%360, 180%360, 180%360], [-55, -55, -70, -70, -55],
-#         color='gray', linewidth=2.5, transform=ccrs.PlateCarree())
+ax1.plot([180%360, 260%360, 260%360, 180%360, 180%360], [-55, -55, -70, -70, -55],
+        color='grey', linewidth=2.5, transform=ccrs.PlateCarree())
+# Extratropical South Pacific box # Jan 19 2026 version
+# sop_lon_mid = (230 + 280) / 2
+# sop_lat_mid = (-40 + -70) / 2
+# ax.plot([230%360, 280%360, 280%360, 230%360, 230%360], [-40, -40, -70, -70, -40],
+#         color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
 # ax.text(sop_lon_mid, sop_lat_mid, 'SOP', color='black', fontsize=22, transform=ccrs.PlateCarree(),
 #         ha='center', va='center')  # Label for SOP
-# ax1.plot([180%360, 260%360, 260%360, 180%360, 180%360], [-55, -55, -70, -70, -55],
-#         color='gray', linewidth=2.5, transform=ccrs.PlateCarree())
+# ax1.plot([230%360, 280%360, 280%360, 230%360, 230%360], [-40, -40, -70, -70, -40],
+#         color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
+# # North Pacific box
+# npac_lon_mid = (175 + 220) / 2
+# npac_lat_mid = (30 + 50) / 2
+# ax.plot([175%360, 220%360, 220%360, 175%360, 175%360], [30, 30, 50, 50, 30],
+#         color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
+# ax.text(npac_lon_mid, npac_lat_mid, 'NPM', color='black', fontsize=22, transform=ccrs.PlateCarree(),
+#         ha='center', va='center')  # Label for NPM 
+# ax1.plot([175%360, 220%360, 220%360, 175%360, 175%360], [30, 30, 50, 50, 30],
+#         color='gray', linewidth=2.8, transform=ccrs.PlateCarree())
 # ax1.text(sop_lon_mid, sop_lat_mid, 'SOP', color='black', fontsize=22, fontweight='bold', transform=ccrs.PlateCarree(),
 #         ha='center', va='center')  # Label for SOP
 # Save the figure
 figure_output = '/work/mh0033/m301036/OBS_LPS_revision/docs/Figs/FIG4/'
 for ext in ("png", "pdf"):
-    fig.savefig(figure_output+f"FIG4_SNgt2_mean_emergence_timescales_update_defined_regions.{ext}", dpi=300, bbox_inches='tight')
+    fig.savefig(figure_output+f"FIG4_SNgt1_median_emergence_timescales.{ext}", dpi=300, bbox_inches='tight')
 
 plt.show()
 # %%
